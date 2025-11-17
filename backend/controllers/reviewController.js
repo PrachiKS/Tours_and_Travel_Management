d923d0aa94299ad4eb974a54b4a0fb9dee2f1501
PrachiKS <prachiksonawane25@gmail.com>
2025-11-17 15:43:14 +0530

    main

---
+++ backend/controllers/reviewController.js
@@ -0,0 +1,27 @@
+
+import { getFeaturedTour } from "./tourController.js";
+import Review from "../models/Review.js"
+import Tour from "../models/Tour.js"
+
+
+
+export const createReview = async (req, res) => {
+
+    const tourId = req.params.tourID
+    const newReview = new Review({ ... req.body })
+
+    try {
+
+        const savedReview = await newReview.save()
+
+        //after creating a new review now update the review array of the tour
+        await Tour.findByIdAndUpdate(tourId, {
+            $push: { reviews: savedReview._id }
+        })
+
+        res.status(200).json({ success: true, message: 'Review submitted', data: savedReview });
+
+    } catch (err) {
+        res.status(500).json({ success: false, message: 'failed to submit' })
+    }
+};
